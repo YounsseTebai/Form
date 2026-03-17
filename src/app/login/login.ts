a0a5ca49e821bb20d63bbca55d2e1a8bb06dddfa
YounsseTebai <tebaiyounsse2009@gmail.com>
2026-03-17 00:55:15 +0000

    login

---
+++ src/app/login/login.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {} from "@angular/material"
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-login',
+  imports: [MatIcon ],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+
+}
